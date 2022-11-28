a9de035021851cdbc7b0b4b5f44965f03e8a1385
sanlorng <fujia625@gmail.com>
2022-11-28 22:59:44 +0800

    Update application icon resources

--- DiskTools.WinUI/DiskTools.WinUI/MainWindow.xaml.cs
+++ DiskTools.WinUI/DiskTools.WinUI/MainWindow.xaml.cs
@@ -1,28 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using ABI.Windows.UI;
-using DiskTools.WinUI.Navigation.Model;
-using DiskTools.WinUI.Pages;
-using DiskTools.WinUI.Pages.Disk;
 using DiskTools.WinUI.Windowing;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -34,8 +15,6 @@ namespace DiskTools.WinUI
     public sealed partial class MainWindow : BaseWindow
     {
 
-        private MainWindowViewModel viewModel = new();
-
         public MainWindow()
         {
             this.InitializeComponent();
@@ -46,17 +25,9 @@ namespace DiskTools.WinUI
             titleBar.BackgroundColor = Colors.Transparent;
             titleBar.ButtonBackgroundColor = Colors.Transparent;
             titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            Title = Windows.ApplicationModel.Package.Current.DisplayName;
         }
 
-        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            if (args.SelectedItem is DiskModel diskModel)
-            {
-                NavigationFrame.Navigate(typeof(DiskPage), diskModel);
-            } else if (args.IsSettingsSelected)
-            {
-                //TODO Navigate Setting Page
-            }
-        }
+        
     }
 }
