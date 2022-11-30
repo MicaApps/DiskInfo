3b650aebcddaa50d5f8b1d0d26c48da5a424ae88
sanlorng <fujia625@gmail.com>
2022-11-30 15:16:25 +0800

    Remove unused code

--- DiskTools.WinUI/DiskTools.WinUI/MainWindow.xaml.cs
+++ DiskTools.WinUI/DiskTools.WinUI/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
 using DiskTools.WinUI.Windowing;
-using Microsoft.UI;
 using Microsoft.UI.Windowing;
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace DiskTools.WinUI
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainWindow : BaseWindow
     {
 
--- DiskTools.WinUI/DiskTools.WinUI/Pages/Disk/DiskPage.xaml.cs
+++ DiskTools.WinUI/DiskTools.WinUI/Pages/Disk/DiskPage.xaml.cs
@@ -1,19 +1,10 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
 using DiskTools.WinUI.Navigation.Model;
 using DiskTools.WinUI.Pages.Disk.Detail;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace DiskTools.WinUI.Pages.Disk
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DiskPage : Page
     {
         private DiskPageViewModel viewModel = new();
