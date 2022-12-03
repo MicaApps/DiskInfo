bfd5657e6c0db9a4e80961e1268432a304f4f4bf
BlameTwo <3014085426@qq.com>
2022-12-03 20:03:46 +0800

    增加依赖注入

--- DiskTools.WinUI/DiskTools.WinUI/Pages/Disk/DiskPage.xaml.cs
+++ DiskTools.WinUI/DiskTools.WinUI/Pages/Disk/DiskPage.xaml.cs
@@ -7,10 +7,15 @@ namespace DiskTools.WinUI.Pages.Disk
 {
     public sealed partial class DiskPage : Page
     {
-        private DiskPageViewModel viewModel = new();
+        private DiskPageViewModel VM
+        {
+            get;
+        }
         public DiskPage()
         {
+            this.VM = App.GetService<DiskPageViewModel>();  
             this.InitializeComponent();
+
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -18,18 +23,18 @@ namespace DiskTools.WinUI.Pages.Disk
             base.OnNavigatedTo(e);
             if (e.Parameter is DiskModel diskModel)
             {
-                viewModel.DiskModel= diskModel;
+                VM.DiskModel= diskModel;
             }
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (ItemList.SelectedItem is ListViewItem item && item.Content is string content && viewModel.DiskModel != null)
+            if (ItemList.SelectedItem is ListViewItem item && item.Content is string content && VM.DiskModel != null)
             {
                 switch(content)
                 {
                     case "Overview":
-                        DiskInfoContent.Navigate(typeof(OverviewPage), viewModel.DiskModel);
+                        DiskInfoContent.Navigate(typeof(OverviewPage), VM.DiskModel);
                         break;
 
                 }
