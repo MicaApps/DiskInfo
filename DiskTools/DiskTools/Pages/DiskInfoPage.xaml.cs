5f16b8d367ce93832bd043ba05632ba30915f5e4
Denissssse <Shomnipotence@hotmail.com>
2023-01-21 13:47:41 +0800

    Update the design of data grid with icons Add SfRadialGauge Add Syncfusion Claim

--- DiskTools/DiskTools/Pages/DiskInfoPage.xaml.cs
+++ DiskTools/DiskTools/Pages/DiskInfoPage.xaml.cs
@@ -2,6 +2,7 @@ using DiskTools.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using DiskInfoLibWinRT;
+using Syncfusion.UI.Xaml.Gauges;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
