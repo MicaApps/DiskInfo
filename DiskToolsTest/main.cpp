0c36292900b78d04711d20d6d646a37e3c3ab89c
Peter <szpeter@Yeah.net>
2023-01-22 14:26:25 +0800

    DiskInfoPage排版，csv刷新部分搬运

--- DiskToolsTest/main.cpp
+++ DiskToolsTest/main.cpp
@@ -10,9 +10,15 @@ int main()
 	auto firmware = obj.Info();
 	auto info1 = firmware.GetAt(2);
 
-	for (auto const& attr : info1.Attributes())
+	for (auto const& obj : info1.Attributes())
 	{
-		std::wcout << attr.Id << "\t" << attr.Name.data() << '\t' << attr.RawValue.data() << '\n';
+		auto attr = obj.as<winrt::DiskInfoLibWinRT::SmartAttribute>();
+		std::wcout << attr.Id.data() << "\t" << attr.Name.data() << '\t' << attr.RawValue.data() << '\n';
 	}
-
+	info1.Update(2);
+	for (auto const& obj : info1.Attributes())
+	{
+		auto attr = obj.as<winrt::DiskInfoLibWinRT::SmartAttribute>();
+		std::wcout << attr.Id.data() << "\t" << attr.Name.data() << '\t' << attr.RawValue.data() << '\n';
+	}
 }
