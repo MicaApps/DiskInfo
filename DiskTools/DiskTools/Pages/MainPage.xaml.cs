3461cbb32dd9c0767595420e6ce2cce2c2c8d5c3
Steven He <hez2010@outlook.com>
2023-01-27 00:21:59 +0900

    Remove syncfusion key

--- DiskTools/DiskTools/Pages/MainPage.xaml.cs
+++ DiskTools/DiskTools/Pages/MainPage.xaml.cs
@@ -1,8 +1,9 @@
-
+﻿
 using CommunityToolkit.WinUI.UI;
 using DiskTools.Controls;
 using DiskTools.Helpers;
 using DiskTools.Pages.SettingPages;
+using DiskTools.Services;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Automation;
@@ -10,10 +11,11 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Windows.ApplicationModel.Resources;
-using Windows.Foundation.Metadata;
 using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -24,18 +26,37 @@ namespace DiskTools.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        #region Properties
         private readonly AppWindow AppWindow = WindowHelper.GetAppWindowForCurrentWindow();
 
         public PageHeader PageHeader => NavigationView.FindDescendant<PageHeader>();
 
+        public string GetAppTitleFromSystem => ResourceLoader.GetForViewIndependentUse()?.GetString("AppName") ?? "Disk Tools";
+
+        private bool isShowHeader = false;
+
+        public bool IsShowHeader
+        {
+            get { return isShowHeader; }
+            set { isShowHeader = value; OnPropertyChanged(); OnPropertyChanged("FrameMargin"); }
+        }
+
+        public Thickness FrameMargin
+        {
+            get { return IsShowHeader ? new Thickness(0) : new Thickness(0); }// new Thickness(-46, 0, -32, -36); }
+        }
+
+        #endregion
+
+        #region Constructors
         public MainPage()
         {
             InitializeComponent();
             UIHelper.MainPage = this;
-            NavigationView.PaneDisplayMode = NavigationViewPaneDisplayMode.Left;
             UIHelper.MainWindow.Backdrop.BackdropTypeChanged += OnBackdropTypeChanged;
+            DiskInfoService.GetInfo();
             if (UIHelper.HasTitleBar)
             {
                 UIHelper.MainWindow.ExtendsContentIntoTitleBar = true;
@@ -45,20 +66,22 @@ namespace DiskTools.Pages
                 AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
                 ActualThemeChanged += (sender, arg) => ThemeHelper.UpdateSystemCaptionButtonColors();
             }
-            UIHelper.MainWindow.SetTitleBar(AppTitleBar);
             AddNavigationMenuItems();
         }
+        #endregion
 
+        #region Initialize
         private void AddNavigationMenuItems()
         {
-            DriveInfo[] allDirves = DriveInfo.GetDrives();
-            foreach (DriveInfo drive in allDirves)
+            int i = 0;
+            foreach (var drive in DiskInfoService.Instance.Info)
             {
-                NavigationViewItem itemGroup = new NavigationViewItem() { Content = drive.Name, Tag = drive.Name, DataContext = drive, Icon = new FontIcon() { Glyph = "\uEDA2" } };
+                NavigationViewItem itemGroup = new NavigationViewItem() { Content = drive.Model, Tag = i, DataContext = drive, Icon = new FontIcon() { Glyph = "\uEDA2" } };
 
-                AutomationProperties.SetName(itemGroup, drive.Name);
+                AutomationProperties.SetName(itemGroup, drive.Model);
 
                 NavigationView.MenuItems.Add(itemGroup);
+                ++i;
             }
         }
 
@@ -68,8 +91,23 @@ namespace DiskTools.Pages
         {
             // Add handler for ContentFrame navigation.
             NavigationViewFrame.Navigated += On_Navigated;
-            NavigationView.SelectedItem = NavigationView.MenuItems[0];
+            NavigationView.SelectedItem = NavigationView.MenuItems.FirstOrDefault();
             NavigationView.PaneDisplayMode = NavigationViewPaneDisplayMode.Auto;
+        }
+        #endregion
+
+        #region 导航
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                _ = NavigationViewFrame.Navigate(typeof(SettingPage), null, args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                string NavItemTag = args.SelectedItemContainer.Tag.ToString();
+                NavigationView_Navigate(NavItemTag, args.RecommendedNavigationTransitionInfo);
+            }
         }
 
         private void NavigationView_Navigate(string NavItemTag, NavigationTransitionInfo TransitionInfo)
@@ -85,170 +123,43 @@ namespace DiskTools.Pages
                 _page = typeof(DiskInfoPage);
                 parameter = NavItemTag;
             }
-            // Get the page type before navigation so you can prevent duplicate
-            // entries in the backstack.
-            Type PreNavPageType = NavigationViewFrame.CurrentSourcePageType;
 
-            // Only navigate if the selected page isn't currently loaded.
-            if (!(_page is null) && !Equals(PreNavPageType, _page))
+            if (!(_page is null))
             {
                 _ = NavigationViewFrame.Navigate(_page, parameter, TransitionInfo);
             }
         }
 
-        private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args) => _ = TryGoBack();
-
-        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            if (args.IsSettingsSelected)
-            {
-                _ = NavigationViewFrame.Navigate(typeof(SettingPage), null, args.RecommendedNavigationTransitionInfo);
-            }
-            else if (args.SelectedItemContainer != null)
-            {
-                string NavItemTag = args.SelectedItemContainer.Tag.ToString();
-                NavigationView_Navigate(NavItemTag, args.RecommendedNavigationTransitionInfo);
-            }
-        }
-
-        private bool TryGoBack()
-        {
-            if (!NavigationViewFrame.CanGoBack)
-            { return false; }
-
-            // Don't go back if the nav pane is overlayed.
-            if (NavigationView.IsPaneOpen &&
-                (NavigationView.DisplayMode == NavigationViewDisplayMode.Compact ||
-                 NavigationView.DisplayMode == NavigationViewDisplayMode.Minimal))
-            { return false; }
-
-            NavigationViewFrame.GoBack();
-            return true;
-        }
-
-        private void On_Navigated(object _, NavigationEventArgs e)
+        private void On_Navigated(object sender, NavigationEventArgs e)
         {
             NavigationView.IsBackEnabled = NavigationViewFrame.CanGoBack;
             if (NavigationViewFrame.SourcePageType == typeof(SettingPage))
             {
                 // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag.
+                IsShowHeader = true;
                 NavigationView.SelectedItem = (NavigationViewItem)NavigationView.SettingsItem;
-                NavigationView.Header = "����";
+                NavigationView.Header = "设置";
             }
             else if (NavigationViewFrame.SourcePageType == typeof(TestPage))
             {
-                NavigationView.Header = "����";
+                IsShowHeader = true;
+                NavigationView.Header = "测试";
             }
             else
             {
+                IsShowHeader = false;
                 if (NavigationViewFrame.SourcePageType == typeof(DiskInfoPage))
                 {
-                    DiskInfoPage page = NavigationViewFrame.Content as DiskInfoPage;
-                    NavigationViewItem item = NavigationView.MenuItems.OfType<NavigationViewItem>().FirstOrDefault((x) => x.Content.ToString() == page.ID);
-                    if (item != null) { NavigationView.SelectedItem = item; }
+                    //DiskInfoPage page = NavigationViewFrame.Content as DiskInfoPage;
+                    //NavigationViewItem item = (NavigationViewItem)NavigationView.MenuItems[page.ID];
+                    //if (item != null) { NavigationView.SelectedItem = item; }
                 }
-                NavigationView.Header = (((NavigationViewItem)NavigationView.SelectedItem)?.Content?.ToString());
+                //NavigationView.Header = (((NavigationViewItem)NavigationView.SelectedItem)?.Content?.ToString());
             }
         }
+        #endregion
 
-        private void NavigationViewControl_PaneClosing(NavigationView sender, NavigationViewPaneClosingEventArgs args)
-        {
-            UpdateAppTitleMargin(sender);
-        }
-
-        private void NavigationViewControl_PaneOpening(NavigationView sender, object args)
-        {
-            UpdateAppTitleMargin(sender);
-        }
-
-        private void NavigationViewControl_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
-        {
-            if (ApiInformation.IsPropertyPresent("Microsoft.UI.Xaml.UIElement", "TranslationTransition"))
-            {
-                AppTitleBar.TranslationTransition = new Vector3Transition();
-
-                AppTitleBar.Translation = sender.DisplayMode == NavigationViewDisplayMode.Minimal &&
-                         sender.IsBackButtonVisible != NavigationViewBackButtonVisible.Collapsed
-                    ? new System.Numerics.Vector3(((float)sender.CompactPaneLength * 2) - 8, 0, 0)
-                    : new System.Numerics.Vector3((float)sender.CompactPaneLength, 0, 0);
-            }
-            else
-            {
-                Thickness currMargin = AppTitleBar.Margin;
-
-                AppTitleBar.Margin = sender.DisplayMode == NavigationViewDisplayMode.Minimal &&
-                             sender.IsBackButtonVisible != NavigationViewBackButtonVisible.Collapsed
-                    ? new Thickness((sender.CompactPaneLength * 2) - 8, currMargin.Top, currMargin.Right, currMargin.Bottom)
-                    : new Thickness(sender.CompactPaneLength, currMargin.Top, currMargin.Right, currMargin.Bottom);
-            }
-
-            UpdateAppTitleMargin(sender);
-            UpdateHeaderMargin(sender);
-        }
-
-        public string GetAppTitleFromSystem => ResourceLoader.GetForViewIndependentUse()?.GetString("AppName") ?? "Disk Tools";
-
-        private void UpdateAppTitleMargin(NavigationView sender)
-        {
-            const int smallLeftIndent = 4, largeLeftIndent = 24;
-
-            if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.UIElement", "TranslationTransition"))
-            {
-                AppTitle.TranslationTransition = new Vector3Transition();
-
-                AppTitle.Translation = sender.DisplayMode == NavigationViewDisplayMode.Minimal || sender.IsPaneOpen
-                    ? new System.Numerics.Vector3(smallLeftIndent, 0, 0)
-                    : new System.Numerics.Vector3(largeLeftIndent, 0, 0);
-            }
-            else
-            {
-                Thickness currMargin = AppTitle.Margin;
-
-                AppTitle.Margin = sender.DisplayMode == NavigationViewDisplayMode.Minimal || sender.IsPaneOpen
-                    ? new Thickness(smallLeftIndent, currMargin.Top, currMargin.Right, currMargin.Bottom)
-                    : new Thickness(largeLeftIndent, currMargin.Top, currMargin.Right, currMargin.Bottom);
-            }
-        }
-
-        private void UpdateHeaderMargin(NavigationView sender)
-        {
-            if (PageHeader != null)
-            {
-                PageHeader.HeaderPadding = sender.DisplayMode == NavigationViewDisplayMode.Minimal
-                    ? (Thickness)Application.Current.Resources["PageHeaderMinimalPadding"]
-                    : (Thickness)Application.Current.Resources["PageHeaderDefaultPadding"];
-
-                if (ApiInformation.IsPropertyPresent("Microsoft.UI.Xaml.UIElement", "TranslationTransition"))
-                {
-                    PageHeader.TranslationTransition = new Vector3Transition();
-
-                    PageHeader.Translation = sender.DisplayMode == NavigationViewDisplayMode.Minimal &&
-                             sender.IsBackButtonVisible != NavigationViewBackButtonVisible.Collapsed
-                        ? new System.Numerics.Vector3(-(float)sender.CompactPaneLength + 8, 0, 0)
-                        : new System.Numerics.Vector3(0, 0, 0);
-                }
-                else
-                {
-                    Thickness currMargin = PageHeader.Margin;
-
-                    PageHeader.Margin = sender.DisplayMode == NavigationViewDisplayMode.Minimal &&
-                             sender.IsBackButtonVisible != NavigationViewBackButtonVisible.Collapsed
-                        ? new Thickness(-sender.CompactPaneLength + 8, currMargin.Top, currMargin.Right, currMargin.Bottom)
-                        : new Thickness(0, currMargin.Top, currMargin.Right, currMargin.Bottom);
-                }
-            }
-        }
-
-        private void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (!UIHelper.HasTitleBar)
-            {
-                int backButtonWidth = NavigationView.DisplayMode == NavigationViewDisplayMode.Minimal ? 96 : 48;
-                RectInt32 Rect = new((ActualWidth - AppTitleBar.ActualWidth + backButtonWidth).GetActualPixel(), 0, AppTitleBar.ActualWidth.GetActualPixel(), AppTitleBar.ActualHeight.GetActualPixel());
-                AppWindow.TitleBar.SetDragRectangles(new RectInt32[] { Rect });
-            }
-        }
-
+        #region 任务进度条
 
         public void ShowProgressBar()
         {
@@ -281,5 +192,84 @@ namespace DiskTools.Pages
             ProgressBar.ShowError = false;
             ProgressBar.ShowPaused = false;
         }
+
+        #endregion
+
+        #region 标题栏
+
+        private void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (!UIHelper.HasTitleBar)
+            {
+                RectInt32 Rect = new((ActualWidth - AppTitleBar.ActualWidth + 48).GetActualPixel(), 0, AppTitleBar.ActualWidth.GetActualPixel(), 32);
+                AppWindow.TitleBar.SetDragRectangles(new RectInt32[] { Rect });
+            }
+        }
+        #endregion
+
+        #region Unused : 返回按钮
+        //private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args) => _ = TryGoBack();
+        //private bool TryGoBack()
+        //{
+        //    if (!NavigationViewFrame.CanGoBack)
+        //    { return false; }
+
+        //    // Don't go back if the nav pane is overlayed.
+        //    if (NavigationView.IsPaneOpen &&
+        //        (NavigationView.DisplayMode == NavigationViewDisplayMode.Compact ||
+        //         NavigationView.DisplayMode == NavigationViewDisplayMode.Minimal))
+        //    { return false; }
+
+        //    NavigationViewFrame.GoBack();
+        //    return true;
+        //}
+        #endregion
+
+        #region Unused : UpdateHeaderMargin
+        //private void UpdateHeaderMargin(NavigationView sender)
+        //{
+        //    if (PageHeader != null)
+        //    {
+        //        PageHeader.HeaderPadding = sender.DisplayMode == NavigationViewDisplayMode.Minimal
+        //            ? (Thickness)Application.Current.Resources["PageHeaderMinimalPadding"]
+        //            : (Thickness)Application.Current.Resources["PageHeaderDefaultPadding"];
+
+        //        if (ApiInformation.IsPropertyPresent("Microsoft.UI.Xaml.UIElement", "TranslationTransition"))
+        //        {
+        //            PageHeader.TranslationTransition = new Vector3Transition();
+
+        //            PageHeader.Translation = sender.DisplayMode == NavigationViewDisplayMode.Minimal &&
+        //                     sender.IsBackButtonVisible != NavigationViewBackButtonVisible.Collapsed
+        //                ? new System.Numerics.Vector3(-(float)sender.CompactPaneLength + 8, 0, 0)
+        //                : new System.Numerics.Vector3(0, 0, 0);
+        //        }
+        //        else
+        //        {
+        //            Thickness currMargin = PageHeader.Margin;
+
+        //            PageHeader.Margin = sender.DisplayMode == NavigationViewDisplayMode.Minimal &&
+        //                     sender.IsBackButtonVisible != NavigationViewBackButtonVisible.Collapsed
+        //                ? new Thickness(-sender.CompactPaneLength + 8, currMargin.Top, currMargin.Right, currMargin.Bottom)
+        //                : new Thickness(0, currMargin.Top, currMargin.Right, currMargin.Bottom);
+        //        }
+        //    }
+        //}
+        #endregion
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        #endregion
     }
 }
